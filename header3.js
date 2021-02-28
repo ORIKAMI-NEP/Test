68900ddd5be7762c883b00fe04d09c3629acad8f
ORIKAMI-NEP <ta.orikami@gmail.com>
2021-03-01 08:37:51 +0900

    色々

--- header3.js
+++ header3.js
@@ -4,3 +4,4 @@ else if (file1 === "an") all = "<h2>" + content2[1][1] + "</h2>";
 else if (file1 === "de") all = "<h2>" + content2[2][1] + "</h2>";
 else all = "<h2>" + content[Number(file1)][Number(file2)] + "</h2>";
 header3.innerHTML += all;
+console.log(file1);
