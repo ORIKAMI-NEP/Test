2413b8fe14661c65377620fd2620397f082897fa
ORIKAMI-NEP <ta.orikami@gmail.com>
2021-03-01 08:41:18 +0900

    色々

--- header3.js
+++ header3.js
@@ -1,4 +1,4 @@
-if (file1 === "in") all = "<h2>" + content[0][1] + "</h2>";
+if (file1 === "in" || file1 === "") all = "<h2>" + content[0][1] + "</h2>";
 else if (file1 === "en") all = "<h2>" + content2[0][1] + "</h2>";
 else if (file1 === "an") all = "<h2>" + content2[1][1] + "</h2>";
 else if (file1 === "de") all = "<h2>" + content2[2][1] + "</h2>";
